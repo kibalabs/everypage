d646a955a7c7c6a589cfda20b6456e7ce3bce018
Krishan Patel <krishan.patel@carbonnv.com>
2020-04-01 21:13:42 +0100

    Small cleanup

--- everypage/src/components/image.tsx
+++ everypage/src/components/image.tsx
@@ -1,24 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// import { useTheme } from '../themes';
-// import { IAtomProps, defaultAtomProps } from './atomProps';
-// import { ThemeType } from '../util';
-
-
-// export interface IImageThemeStatusAction extends ThemeType {
-// }
-
-// export interface IImageThemeStatus extends ThemeType {
-//   default: IImageThemeStatusAction;
-// }
-
-// export interface IImageTheme extends ThemeType {
-//   normal: IImageThemeStatus;
-// }
-
 export interface IStyledImageProps {
-  // theme: IImageTheme;
   fitType: 'crop' | 'scale';
 }
 
@@ -28,19 +11,15 @@ const StyledImage = styled.img<IStyledImageProps>`
   object-fit: ${(props: IStyledImageProps): string => (props.fitType === 'crop' ? 'cover' : 'fill')};
 `;
 
-export interface IImageProps {//extends IAtomProps<IImageTheme> {
+export interface IImageProps {
   source: string;
   alternativeText: string;
   fitType: 'crop' | 'scale';
 }
 
 export const Image = (props: IImageProps): React.ReactElement => {
-  // const theme = props.theme || useTheme('images', props.mode);
   return (
     <StyledImage
-      // id={props.id}
-      // className={`image ${props.className}`}
-      // theme={theme}
       src={props.source}
       alt={props.alternativeText}
       fitType={props.fitType}
@@ -50,6 +29,5 @@ export const Image = (props: IImageProps): React.ReactElement => {
 
 Image.displayName = 'Image';
 Image.defaultProps = {
-  // ...defaultAtomProps,
   fitType: 'scale',
 };
