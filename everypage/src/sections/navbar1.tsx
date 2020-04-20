e12979b1c04e8759f994832fbe51323aa3e4e4a0
Krishan Patel <krishan.patel@carbonnv.com>
2020-04-20 12:22:15 +0100

    small fix for navbar

--- everypage/src/sections/navbar1.tsx
+++ everypage/src/sections/navbar1.tsx
@@ -32,8 +32,8 @@ export const NavBar1 = (props: INavBar1Props): React.ReactElement => {
       background={backgroundConfig}
     >
       <StyledNavigationBar dimensions={dimensions}>
-        <Stack direction={Direction.Horizontal}>
-          {props.logoImageUrl && <Stack.Item><Image source={props.logoImageUrl} alternativeText='logo' /></Stack.Item>}
+        <Stack direction={Direction.Horizontal} isFullHeight={true} isFullWidth={true}>
+          {props.logoImageUrl && <Stack.Item><Image source={props.logoImageUrl} isFullHeight={true} alternativeText='logo' /></Stack.Item>}
           {props.titleText && <Stack.Item><Text mode='header'>{props.titleText}</Text></Stack.Item>}
           <Stack.Item growthFactor={1}><div /></Stack.Item>
           <Stack.Item><Button mode='primary' text='Try now'/></Stack.Item>
