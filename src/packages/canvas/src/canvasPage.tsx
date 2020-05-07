3af6ce3d8a9268c470ed1aa1999c1bc6413899be
Krishan Patel <krishan.patel@carbonnv.com>
2020-05-07 19:09:12 +0100

    Update canvas to not use core components outside the iframe

--- src/packages/canvas/src/canvasPage.tsx
+++ src/packages/canvas/src/canvasPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Frame, { FrameContextConsumer } from 'react-frame-component';
-import { buildTheme, IndexPage, IWebsite, Direction, ThemeProvider, Stack, Text } from '@kibalabs/everypage-core';
+import { buildTheme, IndexPage, IWebsite, Direction, ThemeProvider } from '@kibalabs/everypage-core';
 
 import { JsonEditor } from './jsonEditor';
 import { ErrorBoundary } from './reactCore/errorBoundary';
 import { useObjectLocalStorageState } from './reactCore/useLocalStorageState';
 import { StyleSheetManager } from 'styled-components';
+import { CanvasStack } from './tempCanvasStack';
 
 const defaultSiteContent = require('./site.json');
 
@@ -17,28 +18,25 @@ export const CanvasPage = (): React.ReactElement => {
   const resolvedSiteContent = (siteContent || defaultSiteContent);
 
   const onJsonUpdated = (parsedJson: object): void => {
-    // TODO(krish): verify this is correct (json schema or typing?)
-    try {
-      setSiteContent(parsedJson as { sections: Record<string, any> } & IWebsite);
-    } catch {
-    }
+    setSiteContent(parsedJson);
   }
 
+  // TODO(krish): use core components here again when the bug below is resolved
   // NOTE(krish): both styled components and react-helmet don't work great with iframes
   // https://github.com/styled-components/styled-components/issues/2973
   // https://github.com/nfl/react-helmet/issues/277
   return (
     <ThemeProvider theme={buildTheme()}>
-      <Stack direction={Direction.Horizontal} isFullHeight={true} isFullWidth={true}>
-        <Stack.Item isFullHeight={true}>
-          <Stack direction={Direction.Vertical} isFullHeight={true} isFullWidth={true}>
-            <Text>everypage canvas v{APP_VERSION}</Text>
-            <Stack.Item growthFactor={1} shrinkFactor={1} shouldAllowScrolling={false}>
+      <CanvasStack direction={Direction.Horizontal} isFullHeight={true} isFullWidth={true}>
+        <CanvasStack.Item isFullHeight={true}>
+          <CanvasStack direction={Direction.Vertical} isFullHeight={true} isFullWidth={true}>
+            <span>everypage canvas v{APP_VERSION}</span>
+            <CanvasStack.Item growthFactor={1} shrinkFactor={1} shouldAllowScrolling={false}>
               <JsonEditor json={resolvedSiteContent} onJsonUpdated={onJsonUpdated}/>
-            </Stack.Item>
-          </Stack>
-        </Stack.Item>
-        <Stack.Item isFullHeight={true} growthFactor={1} shrinkFactor={1}>
+            </CanvasStack.Item>
+          </CanvasStack>
+        </CanvasStack.Item>
+        <CanvasStack.Item isFullHeight={true} growthFactor={1} shrinkFactor={1}>
           <Frame style={{height: '100%', width: '100%'}}>
             <FrameContextConsumer>{ frameContext => (
               <StyleSheetManager target={frameContext.document.head}>
@@ -48,8 +46,8 @@ export const CanvasPage = (): React.ReactElement => {
               </StyleSheetManager>
             )}</FrameContextConsumer>
           </Frame>
-        </Stack.Item>
-      </Stack>
+        </CanvasStack.Item>
+      </CanvasStack>
     </ThemeProvider>
   )
 }
---
+++ src/packages/canvas/src/tempCanvasStack.tsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { IMultiAnyChildProps, ISingleAnyChildProps, Direction, Alignment, getFlexItemAlignment, getFlexContentAlignment, useTheme, IDimensionGuide } from '@kibalabs/everypage-core';
+
+  // NOTE(krish): if the child of the stackitem declares 100% height (on vertical stack) it doesnt work on safari unless it has flex-basis: 0 (https://github.com/philipwalton/flexbugs/issues/197)
+
+export interface ICanvasStackItemProps extends ISingleAnyChildProps {
+  id?: string;
+  className: string;
+  growthFactor: number;
+  shrinkFactor: number;
+  baseSize: string;
+  shouldAllowScrolling: boolean;
+  alignment?: Alignment;
+}
+
+class CanvasStackItem extends React.Component<ICanvasStackItemProps> {
+  static displayName = 'CanvasStack.Item';
+  static defaultProps = {
+    className: '',
+    growthFactor: 0,
+    shrinkFactor: 0,
+    // NOTE(krish): see note above
+    baseSize: 'auto',
+    shouldAllowScrolling: false,
+  };
+}
+
+interface IStyledCanvasStackProps {
+  direction: string;
+  childAlignment: Alignment;
+  contentAlignment: Alignment;
+  isFullWidth: boolean;
+  isFullHeight: boolean;
+  shouldAllowScrolling: boolean;
+}
+
+const StyledCanvasStack = styled.div<IStyledCanvasStackProps>`
+  width: ${(props: IStyledCanvasStackProps): string => (props.isFullWidth ? '100%' : 'auto')};
+  height: ${(props: IStyledCanvasStackProps): string => (props.isFullHeight ? '100%' : 'auto')};
+  display: flex;
+  flex-direction: ${(props: IStyledCanvasStackProps): string => (props.direction === Direction.Vertical ? 'column' : 'row')};
+  overflow-y: ${(props: IStyledCanvasStackProps): string => (props.shouldAllowScrolling && props.direction === Direction.Vertical ? 'auto' : (props.isFullHeight ? 'hidden' : 'visible'))};
+  overflow-x: ${(props: IStyledCanvasStackProps): string => (props.shouldAllowScrolling && props.direction === Direction.Horizontal ? 'auto' : (props.isFullWidth ? 'hidden' : 'visible'))};
+  justify-content: ${(props: IStyledCanvasStackProps): string => getFlexContentAlignment(props.contentAlignment)};
+  align-items: ${(props: IStyledCanvasStackProps): string => getFlexItemAlignment(props.childAlignment)};
+`;
+
+interface ICanvasStackProps extends IMultiAnyChildProps {
+  id?: string;
+  className: string;
+  theme?: IDimensionGuide;
+  direction: Direction;
+  childAlignment: Alignment;
+  contentAlignment: Alignment;
+  shouldAddGutters: boolean;
+  shouldAllowScrolling: boolean;
+  isFullWidth: boolean;
+  isFullHeight: boolean;
+}
+
+export const CanvasStack = (props: ICanvasStackProps): React.ReactElement => {
+  const theme = props.theme || useTheme<IDimensionGuide>('dimensions');
+  const realChildren = React.Children.toArray(props.children).filter((child: React.ReactNode): boolean => child !== null && child !== undefined);
+  const children = realChildren.map((child: React.ReactElement, index: number): React.ReactElement<ICanvasStackItemProps> => (
+    child.type !== CanvasStackItem ? <CanvasStackItem key={index}>{ child }</CanvasStackItem> : child
+  ));
+  return (
+    <StyledCanvasStack
+      id={props.id}
+      className={`stack ${props.className}`}
+      direction={props.direction}
+      childAlignment={props.childAlignment}
+      contentAlignment={props.contentAlignment}
+      isFullWidth={props.isFullWidth}
+      isFullHeight={props.isFullHeight}
+      shouldAllowScrolling={props.shouldAllowScrolling}
+    >
+      { children.map((child: React.ReactElement, index: number): React.ReactElement<ICanvasStackItemProps> => (
+        <StyledCanvasStackItem
+          key={index}
+          id={child.props.id}
+          className={`stack-item ${child.props.className}`}
+          growthFactor={child.props.growthFactor}
+          shrinkFactor={child.props.shrinkFactor}
+          baseSize={child.props.baseSize}
+          shouldAllowScrolling={child.props.shouldAllowScrolling}
+          gutterSize={props.shouldAddGutters ? theme.gutterSize : '0'}
+          direction={props.direction}
+          alignment={child.props.alignment}
+        >
+          <StyledCanvasStackItemInner
+            className={`stack-item-inner`}
+            direction={props.direction}
+            alignment={child.props.alignment}
+          >
+            {child.props.children}
+          </StyledCanvasStackItemInner>
+        </StyledCanvasStackItem>
+      ))}
+    </StyledCanvasStack>
+  );
+};
+
+CanvasStack.defaultProps = {
+  className: '',
+  direction: Direction.Vertical,
+  childAlignment: Alignment.Fill,
+  contentAlignment: Alignment.Fill,
+  shouldAllowScrolling: true,
+  shouldAddGutters: false,
+  isFullWidth: false,
+  isFullHeight: false,
+};
+CanvasStack.Item = CanvasStackItem;
+
+interface IStyledCanvasStackItemProps extends ICanvasStackItemProps {
+  direction: Direction;
+  alignment?: Alignment;
+  gutterSize: string;
+}
+
+const StyledCanvasStackItem = styled.div<IStyledCanvasStackItemProps>`
+  display: flex;
+  flex-grow: ${(props: IStyledCanvasStackItemProps): number => props.growthFactor};
+  flex-shrink: ${(props: IStyledCanvasStackItemProps): number => props.shrinkFactor};
+  flex-basis: ${(props: IStyledCanvasStackItemProps): string => props.baseSize};
+  overflow-y: ${(props: IStyledCanvasStackItemProps): string => (props.shouldAllowScrolling && props.direction === Direction.Vertical ? 'auto' : 'hidden')};
+  overflow-x: ${(props: IStyledCanvasStackItemProps): string => (props.shouldAllowScrolling && props.direction === Direction.Horizontal ? 'auto' : 'hidden')};
+  margin-top: ${(props: IStyledCanvasStackItemProps): string => (props.direction === Direction.Vertical ? props.gutterSize : '0')};
+  margin-bottom: ${(props: IStyledCanvasStackItemProps): string => (props.direction === Direction.Vertical ? props.gutterSize : '0')};
+  margin-left: ${(props: IStyledCanvasStackItemProps): string => (props.direction === Direction.Horizontal ? props.gutterSize : '0')};
+  margin-right: ${(props: IStyledCanvasStackItemProps): string => (props.direction === Direction.Horizontal ? props.gutterSize : '0')};
+  align-self: ${(props: IStyledCanvasStackItemProps): string => (props.alignment ? getFlexItemAlignment(props.alignment) : 'auto')};
+`;
+
+interface IStyledCanvasStackItemInnerProps extends ISingleAnyChildProps {
+  direction: Direction;
+  alignment?: Alignment;
+}
+
+const StyledCanvasStackItemInner = styled.div<IStyledCanvasStackItemInnerProps>`
+  flex-grow: 1;
+  width: ${(props: IStyledCanvasStackItemInnerProps): string => (props.direction === Direction.Vertical ? '100%' : 'auto')};
+  height: ${(props: IStyledCanvasStackItemInnerProps): string => (props.direction === Direction.Horizontal ? '100%' : 'auto')};
+`;
