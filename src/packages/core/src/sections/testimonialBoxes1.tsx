604a223be4614721c247db7efca8b8ed60eb527b
Krishan <krishan711@gmail.com>
2020-08-26 22:45:03 +0100

    [0.5.18] Added ability to set icons in testimonial-boxes-1 + ie changes

--- src/packages/core/src/sections/testimonialBoxes1.tsx
+++ src/packages/core/src/sections/testimonialBoxes1.tsx
@@ -11,6 +11,8 @@ interface ITestimonialBoxes1Box {
   author: string;
   url?: string;
   type?: string;
+  iconId?: string;
+  iconColor?: string;
 }
 
 interface ITestimonialBoxes1Props extends ISectionProps {
@@ -20,18 +22,22 @@ interface ITestimonialBoxes1Props extends ISectionProps {
   boxes?: ITestimonialBoxes1Box[];
 }
 
-const getIcon = (boxType: string, brandColor: string): React.ReactElement => {
-  if (boxType === 'producthunt') {
-    return <KibaIcon _color='#DA552F' iconId={'remix-product-hunt'} />;
-  }
-  if (boxType === 'twitter') {
-    return <KibaIcon _color='#1DA1F2' iconId='ion-logo-twitter' />;
-  }
-  return <KibaIcon _color={brandColor} iconId={'ion-chatbox'} />;
-}
-
 export const TestimonialBoxes1 = (props: ITestimonialBoxes1Props): React.ReactElement => {
   const theme: ITheme = useTheme();
+
+  const getIcon = (boxType: string, iconId?: string, iconColor?: string): React.ReactElement => {
+    if (boxType === 'producthunt') {
+      return <KibaIcon _color='#DA552F' iconId={'remix-product-hunt'} />;
+    }
+    if (boxType === 'twitter') {
+      return <KibaIcon _color='#1DA1F2' iconId='ion-logo-twitter' />;
+    }
+    if (boxType === 'linkedin') {
+      return <KibaIcon _color='#2867B2' iconId='ion-logo-linkedin' />;
+    }
+    return <KibaIcon _color={iconColor || theme.colors.brandPrimary} iconId={iconId || 'ion-chatbox'} />;
+  }
+
   return (
     <Section {...props as ISectionProps} className={getClassName(TestimonialBoxes1.displayName, props.className)}>
       <ResponsiveContainingView size={10}>
@@ -44,7 +50,7 @@ export const TestimonialBoxes1 = (props: ITestimonialBoxes1Props): React.ReactEl
                 <Box key={index} mode={props.boxMode} isFullHeight={props.boxMode !== 'card'}>
                   <Stack direction={Direction.Vertical} childAlignment={Alignment.Start} isFullWidth={true}>
                     <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} childAlignment={Alignment.Center} isFullWidth={true} isFullHeight={true} shouldAddGutters={true}>
-                      { getIcon(box.type, theme.colors.brandPrimary) }
+                      { getIcon(box.type, box.iconId, box.iconColor) }
                       <Link target={box.url} text={box.author} isEnabled={!!box.url} />
                     </Stack>
                     <Stack.Item growthFactor={1} shrinkFactor={1} gutterSizeBefore={PaddingSize.Narrow}>
