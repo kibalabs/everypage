8eff73489364bf13e3f3f01fdb8ba898a08aa28f
Krishan Patel <krishan711@gmail.com>
2020-12-07 18:07:21 +0000

    .

--- src/packages/main-new/src/renderer.tsx
+++ src/packages/main-new/src/renderer.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { ReportChunks } from 'react-universal-component';
+import { ChildCapture, HeadRootProvider } from '@kibalabs/everypage-core';
+
 import App from './pages/app';
 
-export const render = (): string => {
-  return ReactDOMServer.renderToString(<App />);
+export const render = (): any => {
+  const chunkNames: string[] = []
+  const headElements = [];
+  const bodyString = ReactDOMServer.renderToString(
+    <ReportChunks report={(chunkName: string) => chunkNames.push(chunkName)}>
+      <HeadRootProvider root={<ChildCapture headElements={headElements}/>}>
+        <App />
+      </HeadRootProvider>
+    </ReportChunks>
+  );
+  console.log('headElements', headElements);
+  return {bodyString, chunkNames};
 };
