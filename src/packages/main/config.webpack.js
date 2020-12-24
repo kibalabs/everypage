719fc5ce552caa5fb434747db6ea4b603c16b31c
Krishan Patel <krishan711@gmail.com>
2020-12-24 13:48:07 +0000

    [0.7.0] replace react-static with own SSR implementation

---
+++ src/packages/main/config.webpack.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = (config) => {
+  config.entry = {
+    'app': [
+      path.join(__dirname, './src/app.tsx'),
+    ],
+    'index': [
+      path.join(__dirname, './src/index.ts'),
+    ],
+  };
+  config.output.filename = '[name].js';
+  return config;
+};
---
+++ src/packages/main/src/app.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Router, IHistory, IRouteProps } from '@kibalabs/core-react';
+import { ITheme } from '@kibalabs/ui-react';
+import { IWebsite, IndexPage } from '@kibalabs/everypage-core';
+
+export interface RouteData {
+  path: string;
+  content: IWebsite;
+  theme: ITheme;
+}
+
+export interface SiteData {
+  routes: RouteData[];
+  notFoundPageContent: IWebsite;
+  notFoundPageTheme: ITheme;
+}
+
+export interface IAppProps {
+  routerHistory?: IHistory;
+}
+
+const siteData: SiteData = __non_webpack_require__('./siteData.json');
+
+export const App = (props: IAppProps) => {
+  return (
+    <Router history={props.routerHistory}>
+      {siteData.routes.map((routeData: RouteData): React.ReactElement<IRouteProps> => (
+        <Route key={routeData.path} path={routeData.path} pageElement={<IndexPage isRehydrating={true} pageContent={routeData.content} pageTheme={routeData.theme} />} />
+      ))}
+      <Route default={true} pageElement={<IndexPage isRehydrating={true} pageContent={siteData.notFoundPageContent} pageTheme={siteData.notFoundPageTheme} />} />
+    </Router>
+  );
+}
+
+export default App;
+
+if (typeof document !== 'undefined') {
+  const target = document.getElementById('root');
+  const renderMethod = target.hasChildNodes() ? ReactDOM.hydrate : ReactDOM.render;
+  const render = (Component: Function) => {
+    renderMethod(<Component />, target);
+  };
+  render(App);
+}
