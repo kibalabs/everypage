2ca3bac36c7b4f88820592b9a14ee3233c5e1139
Krishan Patel <krishan711@gmail.com>
2021-01-16 15:58:05 +0000

    .

--- src/packages/core/src/sections/imageGallery1.tsx
+++ src/packages/core/src/sections/imageGallery1.tsx
@@ -1,66 +1,51 @@
 import React from 'react';
 
 import { getClassName } from '@kibalabs/core';
-import { Alignment, ContainingView, Direction, EqualGrid, ITheme, KibaIcon, PaddingSize, ResponsiveContainingView, ResponsiveField, Spacing, Stack, useTheme } from '@kibalabs/ui-react';
+import { Alignment, Direction, EqualGrid, ITheme, PaddingSize, ResponsiveContainingView, ResponsiveField, Stack, TextAlignment, useTheme, ResponsiveTextAlignmentView } from '@kibalabs/ui-react';
 
 import { ISectionProps, Section } from '.';
 import { LazyMedia, SectionSubtitleText, SectionTitleText } from '../components';
 import { EverypagePaddingSize } from '../internal';
 
 interface IImageGallery1Image {
-  title?: string;
   mediaUrl?: string;
-  iconId?: string;
+  title?: string;
 }
 
 interface IImageGallery1Props extends ISectionProps {
   titleText?: string;
   subtitleText?: string;
-  // boxMode?: string;
-  // boxVariant?: string;
-  boxSizes?: ResponsiveField<number>;
+  imageSizes?: ResponsiveField<number>;
   images?: IImageGallery1Image[];
 }
 
 export const ImageGallery1 = (props: IImageGallery1Props): React.ReactElement => {
   const theme: ITheme = useTheme();
-  // let boxVariant = props.boxVariant;
-  // if (props.boxMode) {
-  //   console.warn('boxMode is deprecated. Please use boxVariant instead');
-  //   boxVariant = props.boxMode;
-  // }
   const sizes = { base: 12, small: 6, medium: 6, large: 4 };
-  if (props.boxSizes) {
-    sizes.base = props.boxSizes.base ? 12 / props.boxSizes.base : sizes.base;
-    sizes.small = props.boxSizes.small ? 12 / props.boxSizes.small : sizes.small;
-    sizes.medium = props.boxSizes.medium ? 12 / props.boxSizes.medium : sizes.medium;
-    sizes.large = props.boxSizes.large ? 12 / props.boxSizes.large : sizes.large;
+  if (props.imageSizes) {
+    sizes.base = props.imageSizes.base ? 12 / props.imageSizes.base : sizes.base;
+    sizes.small = props.imageSizes.small ? 12 / props.imageSizes.small : sizes.small;
+    sizes.medium = props.imageSizes.medium ? 12 / props.imageSizes.medium : sizes.medium;
+    sizes.large = props.imageSizes.large ? 12 / props.imageSizes.large : sizes.large;
   }
   return (
     <Section {...props as ISectionProps} className={getClassName(ImageGallery1.displayName, props.className)}>
       <ResponsiveContainingView sizeResponsive={{ base: 10, extraLarge: 8 }}>
-        <Stack direction={Direction.Vertical} paddingStart={EverypagePaddingSize.SectionTop} paddingEnd={EverypagePaddingSize.SectionBottom}>
-          {props.titleText && <Stack.Item gutterAfter={props.subtitleText ? PaddingSize.Wide : PaddingSize.Wide2}><SectionTitleText text={props.titleText} /></Stack.Item>}
-          {props.subtitleText && <Stack.Item gutterAfter={PaddingSize.Wide2}><SectionSubtitleText text={props.subtitleText} /></Stack.Item>}
-          <EqualGrid childAlignment={Alignment.Fill} shouldAddGutters={true} childSizeResponsive={sizes}>
-            {props.images?.map((imgObj: IImageGallery1Image, index: number): React.ReactElement => (
-              <ContainingView key={index}>
-                {/* <Box key={index} variant={boxVariant} isFullHeight={boxVariant !== 'card'}> */}
-                <Stack direction={Direction.Vertical} contentAlignment={Alignment.Start} childAlignment={Alignment.Center} isFullWidth={true} isFullHeight={true} paddingStart={PaddingSize.Wide} paddingEnd={PaddingSize.Wide} shouldAddGutters={true}>
-                  {imgObj.mediaUrl && <LazyMedia source={imgObj.mediaUrl} alternativeText={imgObj.title || `imgObj ${index} image`} />}
-                  {!imgObj.mediaUrl && imgObj.iconId && <KibaIcon variant='extraLarge' iconId={imgObj.iconId} _color={theme.colors.brandPrimary} />}
-                  <Spacing variant={PaddingSize.Wide} />
-                </Stack>
-                {/* </Box> */}
-              </ContainingView>
-            ))}
-          </EqualGrid>
-        </Stack>
+        <ResponsiveTextAlignmentView alignment={TextAlignment.Center}>
+          <Stack direction={Direction.Vertical} paddingStart={EverypagePaddingSize.SectionTop} paddingEnd={EverypagePaddingSize.SectionBottom}>
+            {props.titleText && <Stack.Item gutterAfter={props.subtitleText ? PaddingSize.Wide : PaddingSize.Wide2}><SectionTitleText text={props.titleText} /></Stack.Item>}
+            {props.subtitleText && <Stack.Item gutterAfter={PaddingSize.Wide2}><SectionSubtitleText text={props.subtitleText} /></Stack.Item>}
+            <EqualGrid childAlignment={Alignment.Fill} shouldAddGutters={true} childSizeResponsive={sizes}>
+              {props.images?.map((image: IImageGallery1Image, index: number): React.ReactElement => (
+                <LazyMedia key={index} source={image.mediaUrl} alternativeText={image.title || `image ${index}`} />
+              ))}
+            </EqualGrid>
+          </Stack>
+        </ResponsiveTextAlignmentView>
       </ResponsiveContainingView>
     </Section>
   );
 };
 ImageGallery1.displayName = 'image-gallery-1';
 ImageGallery1.defaultProps = {
-  boxVariant: 'bordered-unpadded',
 };
