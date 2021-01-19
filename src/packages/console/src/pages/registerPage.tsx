b1c9c1852f261c41bcd1643f689a1c7d53da5359
Krishan Patel <krishan711@gmail.com>
2021-01-19 22:31:38 +0000

    .

--- src/packages/console/src/pages/registerPage.tsx
+++ src/packages/console/src/pages/registerPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { isValidEmail } from '@kibalabs/core';
 import { useHistory } from '@kibalabs/core-react';
-import { Alignment, Box, Button, ContainingView, Direction, Form, InputType, Link, PaddingSize, ResponsiveContainingView, SingleLineInput, Spacing, Stack, Text, TextAlignment, Checkbox } from '@kibalabs/ui-react';
+import { Alignment, Box, Button, Checkbox, ContainingView, Direction, Form, InputType, Link, PaddingSize, ResponsiveContainingView, SingleLineInput, Spacing, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
 import Helmet from 'react-helmet';
 
 import { useGlobals } from '../globalsContext';
