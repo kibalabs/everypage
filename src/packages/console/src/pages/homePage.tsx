848b51f22b523cd0d806c9870cc56651431f0332
Moiyad <moiyadkayda@gmail.com>
2021-01-21 14:34:51 +0000

    .

--- src/packages/console/src/pages/homePage.tsx
+++ src/packages/console/src/pages/homePage.tsx
@@ -3,14 +3,6 @@ import React from 'react';
 
 import { KibaException } from '@kibalabs/core';
 import { useHistory, useInitialization } from '@kibalabs/core-react';
-// import Box from '@material-ui/core/Box';
-// import Button from '@material-ui/core/Button';
-// import Container from '@material-ui/core/Container';
-// import Grid from '@material-ui/core/Grid';
-// import Paper from '@material-ui/core/Paper';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Typography from '@material-ui/core/Typography';
-
 import { Alignment, Box, Button, ContainingView, Direction, EqualGrid, PaddingSize, Spacing, Stack, Text } from '@kibalabs/ui-react';
 import Helmet from 'react-helmet';
 
@@ -21,38 +13,7 @@ import { IPlan } from '../consoleConfig';
 import { Account, Site } from '../everypageClient/resources';
 import { useGlobals } from '../globalsContext';
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//     minHeight: '100%',
-//   },
-//   content: {
-//     flexGrow: 1,
-//     overflow: 'auto',
-//     marginTop: theme.spacing(12),
-//   },
-//   accountBox: {
-//     backgroundColor: 'rgba(255, 255, 255, 0.4)',
-//     padding: theme.spacing(2, 4),
-//     marginBottom: theme.spacing(4),
-//   },
-//   siteCardGrid: {
-//     marginTop: theme.spacing(1),
-//     marginBottom: theme.spacing(1),
-//   },
-//   accountName: {
-//     marginRight: theme.spacing(1),
-//     display: 'inline',
-//   },
-//   accountType: {
-//     marginRight: theme.spacing(4),
-//     display: 'inline',
-//     fontSize: '1em',
-//   },
-// }));
-
 export const HomePage = (): React.ReactElement => {
-  // const classes = useStyles();
   const history = useHistory();
   const { everypageClient, authManager, consoleConfig } = useGlobals();
   const [accounts, setAccounts] = React.useState<Account[] | null | undefined>(undefined);
@@ -141,9 +102,7 @@ export const HomePage = (): React.ReactElement => {
             </Text>
           ) : (
             accounts.map((account: Account, index: number): React.ReactElement => (
-              <Box variant='bordered'>
-                {/* <Paper key={index} elevation={0} className={classes.accountBox}> */}
-                {/* <Box width={1} display='flex' justifyContent='start' alignItems='baseline'> */}
+              <Box variant='bordered' isFullHeight={true} key={index}>
                 <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} childAlignment={Alignment.Center}>
                   <Text variant='header5'>
                     {account.name}
@@ -153,25 +112,20 @@ export const HomePage = (): React.ReactElement => {
                   <Stack.Item growthFactor={1} shrinkFactor={1} />
                   {authManager.getHasJwtPermission(`acc-${account.accountId}-adm`) && <Button onClicked={(): void => onCreateSiteClicked(account)} text='Create site' />}
                 </Stack>
-                {/* </Box> */}
                 <Spacing variant={PaddingSize.Wide} />
-                {accountSites[account.accountId].map((site: Site, innerIndex: number) => (
-                  <EqualGrid contentAlignment={Alignment.Fill} shouldAddGutters={true}>
-                    <SiteCard site={site} onSiteClicked={onSiteClicked} isEnabled={authManager.getHasJwtPermission(`st-${site.siteId}-vw`)} />
-                  </EqualGrid>
-                ))}
-                {accountSites[account.accountId].length === 0 && (
-                  <EqualGrid contentAlignment={Alignment.Start}>
+                <EqualGrid childSizeResponsive={{ small: 4, base: 3 }} contentAlignment={Alignment.Start} shouldAddGutters={true}>
+                  {accountSites[account.accountId].map((site: Site, innerIndex: number) => (
+                    <SiteCard key={innerIndex} site={site} onSiteClicked={onSiteClicked} isEnabled={authManager.getHasJwtPermission(`st-${site.siteId}-vw`)} />
+                  ))}
+                  {accountSites[account.accountId].length === 0 && (
                     <Text variant='header6-note'>
                       {'No sites yet. Create one now!'}
                     </Text>
-                  </EqualGrid>
-                )}
-                {/* </Paper> */}
+                  )}
+                </EqualGrid>
               </Box>
             ))
           )}
-          {/* </Container> */}
         </Stack>
       </ContainingView>
       {accountUpgradePopupAccount && <AccountUpgradeDialog isOpen={true} account={accountUpgradePopupAccount} onCloseClicked={onAccountUpgradePopupCloseClicked} onUpgradeClicked={onAccountUpgradePopupUpgradeClicked} />}
