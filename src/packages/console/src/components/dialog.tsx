60db95a1125728034c50e502824fee79f62da610
Krishan Patel <krishan711@gmail.com>
2021-02-01 21:35:08 +0000

    .

---
+++ src/packages/console/src/components/dialog.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { ISingleAnyChildProps, useEventListener } from "@kibalabs/core-react";
+import { Box } from '@kibalabs/ui-react';
+import styled from 'styled-components';
+
+const StyledBackdrop = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  left: 0;
+  top: 0;
+`;
+
+interface IDialogProps extends ISingleAnyChildProps {
+isOpen: boolean;
+onCloseClicked: () => void;
+}
+
+export const Dialog = (props: IDialogProps): React.ReactElement | null => {
+  const dialogRef = React.useRef();
+
+  const onBackdropClicked = (event: React.SyntheticEvent<HTMLDivElement>) => {
+    if (event.target === dialogRef.current) {
+      props.onCloseClicked();
+    }
+  };
+
+  useEventListener(document, 'keydown', (event: Event): void => {
+    // NOTE(krishan711): this doesn't pass the dependencies in as it should
+    if (props.isOpen && event.key === 'Escape') {
+      props.onCloseClicked();
+    }
+  });
+
+  return props.isOpen ? (
+    <StyledBackdrop id='backdrop' ref={dialogRef} onClick={onBackdropClicked}>
+      <Box variant='card' maxWidth='30%' maxHeight='70%'>
+        {props.children}
+      </Box>
+    </StyledBackdrop>
+  ) : null;
+};
--- src/packages/console/src/components/messageDialog.tsx
+++ src/packages/console/src/components/messageDialog.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { Alignment, Button, Direction, PaddingSize, Stack, Text, Box, TextAlignment } from '@kibalabs/ui-react';
-import styled from 'styled-components';
-import { ISingleAnyChildProps, useEventListener } from '@kibalabs/core-react';
+import { Button, Direction, PaddingSize, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
+import { Dialog } from './dialog';
 
 export interface IMessageDialogProps {
   isOpen: boolean;
@@ -14,11 +13,10 @@ export interface IMessageDialogProps {
 }
 
 export const MessageDialog = (props: IMessageDialogProps): React.ReactElement => {
-
   const confirmButtonText = props.confirmButtonText || 'Confirm';
 
   return (
-    <Modal isOpen={props.isOpen} onCloseClicked={props.onCloseClicked}>
+    <Dialog isOpen={props.isOpen} onCloseClicked={props.onCloseClicked}>
       <Stack direction={Direction.Vertical} shouldAddGutters={true} defaultGutter={PaddingSize.Wide}>
         <Text variant='header3' alignment={TextAlignment.Center}>{props.title}</Text>
         <Text alignment={TextAlignment.Center}>{props.message}</Text>
@@ -31,51 +29,6 @@ export const MessageDialog = (props: IMessageDialogProps): React.ReactElement =>
           </Stack>
         </Stack.Item>
       </Stack>
-    </Modal>
+    </Dialog>
   );
-};
-
-MessageDialog.defaultProps = {
-};
-
-interface IModalProps extends ISingleAnyChildProps {
-  isOpen: boolean;
-  onCloseClicked: () => void;
-}
-
-const StyledBackdrop = styled.div`
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.8);
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  left: 0;
-  top: 0;
-`;
-
-const Modal = (props: IModalProps): React.ReactElement | null => {
-  const modalRef = React.useRef();
-
-  const onBackdropClicked = (event: React.SyntheticEvent<HTMLDivElement>) => {
-    if (event.target === modalRef.current) {
-      props.onCloseClicked();
-    }
-  };
-
-  useEventListener(document, 'keydown', (event: Event): void => {
-    // NOTE(krishan711): this doesn't pass the dependencies in as it should
-    if (props.isOpen && event.key === 'Escape') {
-      props.onCloseClicked();
-    }
-  });
-
-  return props.isOpen ? (
-    <StyledBackdrop id='backdrop' ref={modalRef} onClick={onBackdropClicked}>
-      <Box variant='card' maxWidth='30%' maxHeight='70%'>
-        {props.children}
-      </Box>
-    </StyledBackdrop>
-  ) : null;
 };
