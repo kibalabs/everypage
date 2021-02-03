8114c3a5afb57b345fd4a0e3bda0f6d6215c703d
Krishan Patel <krishan711@gmail.com>
2021-02-03 17:24:36 +0000

    .

--- src/packages/console/src/components/newPlanDialog.tsx
+++ src/packages/console/src/components/newPlanDialog.tsx
@@ -32,46 +32,17 @@ export const NewPlanDialog = (props: INewPlanDialogProps): React.ReactElement =>
     <Dialog isOpen={true} onCloseClicked={onCloseClicked}>
       <Stack direction={Direction.Vertical} shouldAddGutters={true} defaultGutter={PaddingSize.Wide}>
         <Text variant='header3' alignment={TextAlignment.Center}>{`${newPlan.planIndex > currentPlan.planIndex ? 'Upgrade' : 'Downgrade'} to ${newPlan.name}`}</Text>
-        <Stack direction={Direction.Vertical}>
+        <Stack direction={Direction.Vertical} shouldAddGutters={true}>
           {newPlan.planIndex < currentPlan.planIndex && (
-            <Text>
-              If you downgrade we may have to remove some sites and other features from your existing sites to meet the new quotas 😢
-              <Spacing variant={PaddingSize.Default} />
-              If we can help you get more value out of your current plan instead, just reach out to us, we are always open to feedback 👀
-              <Spacing variant={PaddingSize.Default} />
-              If you are sure you want to do this just click downgrade and we will email you to confirm next steps.
-            </Text>
+            <React.Fragment>
+              <Text>If you downgrade we may have to remove some sites and other features from your existing sites to meet the new quotas 😢</Text>
+              <Text>If we can help you get more value out of your current plan instead, just reach out to us, we are always open to feedback 👀</Text>
+              <Text>If you are sure you want to do this just click downgrade and we will email you to confirm next steps.</Text>
+            </React.Fragment>
           )}
           {newPlan.planIndex > currentPlan.planIndex && (
             <React.Fragment>
               <Text>We are so glad you are enjoying everypage 🙌</Text>
-              <Spacing />
-              {currentPlan.code === 'core' ? (
-                <React.Fragment>
-                  <Spacing variant={PaddingSize.Narrow} />
-                  <Text>Since you have not got a current subscription for this account, we will need your credit card details to continue.</Text>
-                  <TextField
-                    variant='outlined'
-                    margin='normal'
-                    fullWidth
-                    label='Card Details'
-                    disabled={isLoading}
-                    InputLabelProps={{ shrink: true }}
-                    InputProps={{
-                      inputComponent: StripeInput,
-                      inputProps: {
-                        component: CardElement,
-                      },
-                    }}
-                    error={upgradeCardError !== undefined}
-                    helperText={upgradeCardError}
-                  />
-                  <Text variant='note'>Secured by Stripe</Text>
-                </React.Fragment>
-              ) : (
-                <Text>When upgrading, you will not be charged straight away - your next bill will just include a pro-rated amount to pay for the remaining time in this month, so you can start using your new powers immediately 🥳.</Text>
-              )}
-              <br />
               <TextField
                 variant='outlined'
                 margin='normal'
@@ -84,6 +55,31 @@ export const NewPlanDialog = (props: INewPlanDialogProps): React.ReactElement =>
                 error={upgradeDiscountCodeError !== undefined}
                 helperText={upgradeDiscountCodeError}
               />
+              {currentPlan.code === 'core' ? (
+                <React.Fragment>
+                  <Stack.Item gutterAfter={PaddingSize.None}>
+                    <TextField
+                      variant='outlined'
+                      margin='normal'
+                      fullWidth
+                      label='Card Details'
+                      disabled={isLoading}
+                      InputLabelProps={{ shrink: true }}
+                      InputProps={{
+                        inputComponent: StripeInput,
+                        inputProps: {
+                          component: CardElement,
+                        },
+                      }}
+                      error={upgradeCardError !== undefined}
+                      helperText={upgradeCardError}
+                    />
+                  </Stack.Item>
+                  <Text variant='note'>Secured by Stripe</Text>
+                </React.Fragment>
+              ) : (
+                <Text>When upgrading, you will not be charged straight away - your next bill will just include a pro-rated amount to pay for the remaining time in this month, so you can start using your new powers immediately 🥳.</Text>
+              )}
               {upgradeError && <Text variant='error'>{upgradeError}</Text>}
             </React.Fragment>
           )}
