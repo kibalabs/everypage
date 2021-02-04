70fbb15afcf75dd8e18e7cb09d1743652cfbf29d
Moiyad <moiyadkayda@gmail.com>
2021-02-04 17:50:59 +0000

    .

--- src/packages/core/src/sections/contact2.tsx
+++ src/packages/core/src/sections/contact2.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { getClassName } from '@kibalabs/core';
-import { Alignment, Button, Direction, Form, InputType, KibaIcon, MultiLineInput, PaddingSize, ResponsiveContainingView, ResponsiveTextAlignmentView, SingleLineInput, Spacing, Stack, Text, TextAlignment, MarkdownText } from '@kibalabs/ui-react';
+import { Alignment, Button, Direction, EqualGrid, Form, InputType, KibaIcon, MarkdownText, MultiLineInput, PaddingSize, ResponsiveContainingView, ResponsiveTextAlignmentView, SingleLineInput, Spacing, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
 
 import { ISectionProps, Section } from '.';
 import { SectionSubtitleText, SectionTitleText } from '../components';
@@ -104,90 +104,92 @@ export const Contact2 = (props: IContact2Props): React.ReactElement => {
       setErrorMessage(result.responseMessage);
     }
   };
-
+  const sizes = { base: 12, small: 12, medium: 6, large: 4 };
   return (
     <Section {...props as ISectionProps} className={getClassName(Contact2.displayName, props.className)}>
       <ResponsiveContainingView sizeResponsive={{ base: 10 }}>
-        <Stack direction={Direction.Horizontal}>
-          <Stack direction={Direction.Vertical} paddingStart={EverypagePaddingSize.SectionTop} paddingEnd={EverypagePaddingSize.SectionBottom} shouldAddGutters={true}>
-            {props.titleText && <Stack.Item gutterAfter={props.subtitleText ? PaddingSize.Wide : PaddingSize.Wide2}><SectionTitleText text={props.titleText} /></Stack.Item>}
-            {props.subtitleText && <Stack.Item gutterAfter={PaddingSize.Wide2}><SectionSubtitleText text={props.subtitleText} /></Stack.Item>}
-            {props.address && (
-              <Stack.Item gutterAfter={PaddingSize.Wide}>
-                <MarkdownText source={props.address} />
-              </Stack.Item>
-            )}
-            {props.phoneNumber && (
-              <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} shouldAddGutters={true}>
-                <KibaIcon iconId='ion-call-outline' />
-                <Text>{props.phoneNumber}</Text>
-              </Stack>
-            )}
-            {props.email && (
-              <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} shouldAddGutters={true}>
-                <KibaIcon iconId='ion-mail-outline' />
-                <Text>{props.email}</Text>
-              </Stack>
-            )}
-            <Stack.Item growthFactor={20} shrinkFactor={1} />
-            {props.bottomText && <Stack.Item><SectionSubtitleText text={props.bottomText} /></Stack.Item>}
-          </Stack>
-          <ResponsiveTextAlignmentView alignment={TextAlignment.Center}>
-            <Stack direction={Direction.Vertical} paddingStart={EverypagePaddingSize.SectionTop} paddingEnd={EverypagePaddingSize.SectionBottom}>
-              <Form onFormSubmitted={onFormSubmitted} isLoading={isLoading}>
-                <Stack direction={Direction.Vertical} shouldAddGutters={true} defaultGutter={PaddingSize.Wide1}>
-                  <Stack.Item growthFactor={1} shrinkFactor={1} />
-                  <SingleLineInput
-                    inputWrapperVariant={nameError ? 'error' : ''}
-                    inputType={InputType.Text}
-                    name='name'
-                    placeholderText='Name'
-                    value={name}
-                    onValueChanged={onNameChanged}
-                    messageText={nameError}
-                  />
-                  <Stack directionResponsive={{ base: Direction.Vertical, medium: Direction.Horizontal }} contentAlignment={Alignment.Start} shouldAddGutters={true} defaultGutter={PaddingSize.Narrow}>
-                    <Stack.Item growthFactor={1} shrinkFactor={1}>
-                      <SingleLineInput
-                        inputWrapperVariant={emailError ? 'error' : ''}
-                        inputType={InputType.Email}
-                        name='email'
-                        placeholderText='Email'
-                        value={email}
-                        onValueChanged={onEmailChanged}
-                        messageText={emailError}
-                      />
-                    </Stack.Item>
-                    <Stack.Item><Spacing /></Stack.Item>
-                    <Stack.Item growthFactor={1} shrinkFactor={1}>
-                      <SingleLineInput
-                        inputWrapperVariant={phoneNumberError ? 'error' : ''}
-                        inputType={InputType.PhoneNumber}
-                        name='phone-number'
-                        placeholderText='Phone Number'
-                        value={phoneNumber}
-                        onValueChanged={onPhoneNumberChanged}
-                        messageText={phoneNumberError}
-                      />
-                    </Stack.Item>
-                  </Stack>
-                  <MultiLineInput
-                    inputWrapperVariant={errorMessage || messageError ? 'error' : successMessage ? 'success' : ''}
-                    placeholderText='Message'
-                    value={message}
-                    onValueChanged={onMessageChanged}
-                    messageText={errorMessage || messageError || successMessage}
-                  />
-                  <Button
-                    variant='primary'
-                    buttonType='submit'
-                    text={inputButtonText}
-                    isLoading={isLoading}
-                  />
+        <Stack direction={Direction.Vertical} paddingStart={EverypagePaddingSize.SectionTop} paddingEnd={EverypagePaddingSize.SectionBottom}>
+          <EqualGrid childSizeResponsive={sizes} contentAlignment={Alignment.Center}>
+            <Stack direction={Direction.Vertical} contentAlignment={Alignment.Center} shouldAddGutters={true}>
+              {props.titleText && <Stack.Item gutterAfter={props.subtitleText ? PaddingSize.Wide : PaddingSize.Wide2}><SectionTitleText text={props.titleText} /></Stack.Item>}
+              {props.subtitleText && <Stack.Item gutterAfter={PaddingSize.Wide2}><SectionSubtitleText text={props.subtitleText} /></Stack.Item>}
+              {props.address && (
+                <Stack.Item gutterAfter={PaddingSize.Wide}>
+                  <MarkdownText source={props.address} />
+                </Stack.Item>
+              )}
+              {props.phoneNumber && (
+                <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} childAlignment={Alignment.Center} shouldAddGutters={true}>
+                  <KibaIcon iconId='ion-call-outline' />
+                  <Text>{props.phoneNumber}</Text>
                 </Stack>
-              </Form>
+              )}
+              {props.email && (
+                <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} childAlignment={Alignment.Center} shouldAddGutters={true}>
+                  <KibaIcon iconId='ion-mail-outline' />
+                  <Text>{props.email}</Text>
+                </Stack>
+              )}
+              <Stack.Item growthFactor={20} shrinkFactor={1} />
+              {props.bottomText && <Stack.Item><SectionSubtitleText text={props.bottomText} /></Stack.Item>}
             </Stack>
-          </ResponsiveTextAlignmentView>
+            <ResponsiveTextAlignmentView alignment={TextAlignment.Center}>
+              <Stack direction={Direction.Vertical}>
+                <Form onFormSubmitted={onFormSubmitted} isLoading={isLoading}>
+                  <Stack direction={Direction.Vertical} shouldAddGutters={true} defaultGutter={PaddingSize.Wide1}>
+                    <Stack.Item growthFactor={1} shrinkFactor={1} />
+                    <SingleLineInput
+                      inputWrapperVariant={nameError ? 'error' : ''}
+                      inputType={InputType.Text}
+                      name='name'
+                      placeholderText='Name'
+                      value={name}
+                      onValueChanged={onNameChanged}
+                      messageText={nameError}
+                    />
+                    <Stack directionResponsive={{ base: Direction.Vertical, medium: Direction.Horizontal }} contentAlignment={Alignment.Start} shouldAddGutters={true} defaultGutter={PaddingSize.Narrow}>
+                      <Stack.Item growthFactor={1} shrinkFactor={1}>
+                        <SingleLineInput
+                          inputWrapperVariant={emailError ? 'error' : ''}
+                          inputType={InputType.Email}
+                          name='email'
+                          placeholderText='Email'
+                          value={email}
+                          onValueChanged={onEmailChanged}
+                          messageText={emailError}
+                        />
+                      </Stack.Item>
+                      <Stack.Item><Spacing /></Stack.Item>
+                      <Stack.Item growthFactor={1} shrinkFactor={1}>
+                        <SingleLineInput
+                          inputWrapperVariant={phoneNumberError ? 'error' : ''}
+                          inputType={InputType.PhoneNumber}
+                          name='phone-number'
+                          placeholderText='Phone Number'
+                          value={phoneNumber}
+                          onValueChanged={onPhoneNumberChanged}
+                          messageText={phoneNumberError}
+                        />
+                      </Stack.Item>
+                    </Stack>
+                    <MultiLineInput
+                      inputWrapperVariant={errorMessage || messageError ? 'error' : successMessage ? 'success' : ''}
+                      placeholderText='Message'
+                      value={message}
+                      onValueChanged={onMessageChanged}
+                      messageText={errorMessage || messageError || successMessage}
+                    />
+                    <Button
+                      variant='primary'
+                      buttonType='submit'
+                      text={inputButtonText}
+                      isLoading={isLoading}
+                    />
+                  </Stack>
+                </Form>
+              </Stack>
+            </ResponsiveTextAlignmentView>
+          </EqualGrid>
         </Stack>
       </ResponsiveContainingView>
     </Section>
