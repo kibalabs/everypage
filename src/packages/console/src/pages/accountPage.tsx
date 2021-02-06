e9bc10cb22f144517b4bdc30378cd1bc20a95dc4
MoiyadKayda <42097538+MoiyadKayda@users.noreply.github.com>
2021-02-06 21:03:24 +0530

    Feature - Convert AccountPage to use ui-react (#39)
    * .
    
    * .
    
    * account page uses ui-react
    
    * .
    
    * Update src/packages/console/src/pages/accountPage.tsx
    
    default is different from primary
    
    Co-authored-by: Krishan Patel <krishan711@gmail.com>
    
    * Update src/packages/console/src/pages/accountPage.tsx
    
    Co-authored-by: Krishan Patel <krishan711@gmail.com>
    
    * Update src/packages/console/src/pages/accountPage.tsx
    
    Co-authored-by: Krishan Patel <krishan711@gmail.com>
    
    * .
    
    * .
    
    Co-authored-by: Krishan Patel <krishan711@gmail.com>

--- src/packages/console/src/pages/accountPage.tsx
+++ src/packages/console/src/pages/accountPage.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 
-
 import { KibaException } from '@kibalabs/core';
 import { useInitialization, useNavigator } from '@kibalabs/core-react';
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import { Alignment, Box, Button, Direction, Grid, PaddingSize, ResponsiveContainingView, Stack, Text } from '@kibalabs/ui-react';
 import { CardElement, Elements } from '@stripe/react-stripe-js';
 import { loadStripe, PaymentIntent, Stripe, StripeElements, StripeError } from '@stripe/stripe-js';
 import Helmet from 'react-helmet';
@@ -27,68 +20,11 @@ import { useGlobals } from '../globalsContext';
 const stripePromise = loadStripe('pk_live_74pJIhvxX0m61Ub6NDjFiFBy00Q8aDg61J');
 // const stripePromise = loadStripe('pk_test_51GqarKBhdc2gIBl2s6qZ2AUFhlRXQOE0l7y4dnUC5YUoKdLSpobrz3h4hFC3PJduu91lTvWJrPW6YwdrCzxExljh00YB1xWyma');
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    minHeight: '100%',
-  },
-  content: {
-    flexGrow: 1,
-    overflow: 'auto',
-    marginTop: theme.spacing(12),
-  },
-  paper: {
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    padding: theme.spacing(2, 4),
-    marginBottom: theme.spacing(4),
-  },
-  siteCardGrid: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-  accountName: {
-    marginRight: theme.spacing(1),
-  },
-  accountType: {
-  },
-  paperTitle: {
-    fontWeight: 'bold',
-    display: 'inline',
-    marginRight: theme.spacing(4),
-  },
-  planBoxHolder: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  planBox: {
-    padding: theme.spacing(2, 4),
-    margin: theme.spacing(0, 2),
-    flexGrow: 1,
-  },
-  planBoxTitle: {
-    fontWeight: 'bold',
-  },
-  planPrice: {
-    fontWeight: 'bold',
-    fontSize: '3em',
-    marginTop: theme.spacing(2),
-  },
-  planButton: {
-    marginTop: theme.spacing(2),
-  },
-  planCurrentText: {
-    textAlign: 'center',
-    marginTop: theme.spacing(2),
-  },
-}));
-
 export interface IAccountPageProps {
   accountId: string;
 }
 
 export const AccountPage = (props: IAccountPageProps): React.ReactElement => {
-  const classes = useStyles();
   const navigator = useNavigator();
   const { everypageClient, authManager, consoleConfig } = useGlobals();
   const [account, setAccount] = React.useState<Account | null | undefined>(undefined);
@@ -236,112 +172,119 @@ export const AccountPage = (props: IAccountPageProps): React.ReactElement => {
   };
 
   return (
-    <div className={classes.root}>
-      <Elements stripe={stripePromise}>
-        <NavigationBar />
-        <Helmet>
-          <title>{`${account ? account.name : 'Account page'} | Everypage Console`}</title>
-        </Helmet>
-        <main className={classes.content}>
-          <Container maxWidth='lg'>
-            {account === undefined || accountSites === undefined ? (
-              <Typography component='p'>loading...</Typography>
-            ) : account === null || accountSites === null ? (
-              <Typography component='p'>An error occurred. Please try again later.</Typography>
-            ) : (
-              <React.Fragment>
-                <Paper elevation={0} className={classes.paper}>
-                  <Typography variant='h5' className={classes.accountName}>{account.name}</Typography>
-                  <br />
-                  <Typography>
+    <Elements stripe={stripePromise}>
+      <NavigationBar />
+      <Helmet>
+        <title>{`${account ? account.name : 'Account page'} | Everypage Console`}</title>
+      </Helmet>
+      <ResponsiveContainingView size={12}>
+        <Stack direction={Direction.Vertical} isFullWidth={true} paddingTop={PaddingSize.Wide4} paddingBottom={PaddingSize.Wide2} isScrollableHorizontally={false}>
+          {account === undefined || accountSites === undefined ? (
+            <Text tag='p'>loading...</Text>
+          ) : account === null || accountSites === null ? (
+            <Text tag='p'>An error occurred. Please try again later.</Text>
+          ) : (
+            <React.Fragment>
+              <Box variant='card'>
+                <Stack direction={Direction.Vertical} shouldAddGutters={true} defaultGutter={PaddingSize.Default}>
+                  <Text variant='header5'>{account.name}</Text>
+                  <Text>
                     {' You are currently on the '}
-                    <b>{currentPlan.name}</b>
+                    <Text variant='bold'>{currentPlan.name}</Text>
                     {' plan'}
-                  </Typography>
+                  </Text>
                   {nextPlan && (
-                    <Typography variant='caption'>
-                      <Button color='primary' size='small' onClick={(): void => onChangePlanClicked(nextPlan)}>Upgrade</Button>
-                      {' to '}
-                      <b>{nextPlan.name}</b>
-                      {` to ${nextPlan.highlightFeature} and more 🚀`}
-                    </Typography>
+                    <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} childAlignment={Alignment.Center} shouldAddGutters={true} defaultGutter={PaddingSize.Default}>
+                      <Button variant='small-default' onClicked={(): void => onChangePlanClicked(nextPlan)} text='Upgrade' />
+                      <Text variant='note'> to </Text>
+                      <Text variant='note-bold'>{nextPlan.name}</Text>
+                      <Text variant='note'>{` to ${nextPlan.highlightFeature} and more 🚀`}</Text>
+                    </Stack>
                   )}
-                </Paper>
-                <Paper elevation={0} className={classes.paper}>
-                  <Box width={1} display='flex' justifyContent='start' alignItems='baseline'>
-                    <Typography variant='h6' className={classes.paperTitle}>Sites</Typography>
-                    {authManager.getHasJwtPermission(`acc-${account.accountId}-adm`) && <Button color='primary' onClick={onCreateSiteClicked}>Create site</Button>}
-                  </Box>
-                  <Typography>{`${accountSites.length} sites`}</Typography>
-                  <Grid container spacing={2} className={classes.siteCardGrid}>
-                    {accountSites.map((site: Site, innerIndex: number): React.ReactElement => (
-                      <Grid item xs={12} sm={6} md={4} lg={3} key={innerIndex}>
-                        <SiteCard site={site} onSiteClicked={onSiteClicked} isEnabled={authManager.getHasJwtPermission(`st-${site.siteId}-vw`)} />
-                      </Grid>
-                    ))}
-                    {accountSites.length === 0 && (
-                      <Grid item xs={12}>
-                        <Typography color='textSecondary'>
-                          {'No sites yet. Create one now!'}
-                        </Typography>
-                      </Grid>
-                    )}
-                  </Grid>
-                </Paper>
-                <Paper id='plan' elevation={0} className={classes.paper}>
-                  <Box width={1} display='flex' mb={1} justifyContent='start' alignItems='baseline'>
-                    <Typography variant='h6' className={classes.paperTitle}>Subscription</Typography>
-                    <Button color='primary' onClick={onManageWithStripeClicked}>Manage with Stripe</Button>
-                  </Box>
-                  <Box className={classes.planBoxHolder}>
-                    {consoleConfig.plans.map((plan: IPlan, index: number): React.ReactElement => {
-                      if (!plan.isPurchasable) {
-                        return null;
-                      }
-                      return (
-                        <Paper key={index} className={classes.planBox}>
-                          <Typography variant='h6' className={classes.planBoxTitle}>{plan.name}</Typography>
-                          <Typography variant='caption'>{plan.highlightFeature.toUpperCase()}</Typography>
-                          <Typography color='primary' className={classes.planPrice}>{`$${plan.priceMonthly / 100}`}</Typography>
-                          <Typography variant='caption'>per month</Typography>
-                          {plan.planIndex < currentPlan.planIndex && <Button variant='outlined' fullWidth={true} className={classes.planButton} onClick={(): void => onChangePlanClicked(plan)}>Downgrade</Button>}
-                          {plan.planIndex === currentPlan.planIndex && <Button disabled={true} variant='outlined' fullWidth={true} className={classes.planButton} onClick={(): void => onChangePlanClicked(plan)}>Your plan</Button>}
-                          {plan.planIndex > currentPlan.planIndex && <Button color='primary' variant='contained' fullWidth={true} className={classes.planButton} onClick={(): void => onChangePlanClicked(plan)}>Upgrade</Button>}
-                        </Paper>
-                      );
-                    })}
-                  </Box>
-                </Paper>
-              </React.Fragment>
-            )}
-          </Container>
-        </main>
+                </Stack>
+              </Box>
+              <Box variant='card'>
+                <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} childAlignment={Alignment.Center} shouldAddGutters={true} defaultGutter={PaddingSize.Wide} paddingHorizontal={PaddingSize.Wide} paddingBottom={PaddingSize.Default}>
+                  <Text variant='header6'>Sites</Text>
+                  {authManager.getHasJwtPermission(`acc-${account.accountId}-adm`) && (
+                    <Button variant='default' onClicked={onCreateSiteClicked} text='Create site' />
+                  )}
+                  <Stack.Item growthFactor={1} shrinkFactor={1} />
+                  <Text>{`${accountSites.length} sites`}</Text>
+                </Stack>
+                <Grid contentAlignment={Alignment.Start} shouldAddGutters={true} paddingHorizontal={PaddingSize.Wide} paddingVertical={PaddingSize.Wide}>
+                  {accountSites.map((site: Site, innerIndex: number) => (
+                    <Grid.Item sizeResponsive={{ base: 12, small: 6, medium: 4, large: 3 }} key={innerIndex}>
+                      <SiteCard site={site} onSiteClicked={onSiteClicked} isEnabled={authManager.getHasJwtPermission(`st-${site.siteId}-vw`)} />
+                    </Grid.Item>
+                  ))}
+                  {accountSites.length === 0 && (
+                    <Grid.Item size={12}>
+                      <Text variant='colored'>No sites yet. Create one now!</Text>
+                    </Grid.Item>
+                  )}
+                </Grid>
+              </Box>
+              <Box variant='card'>
+                <Stack direction={Direction.Horizontal} contentAlignment={Alignment.Start} childAlignment={Alignment.Center} paddingHorizontal={PaddingSize.Wide} paddingBottom={PaddingSize.Default}>
+                  <Text variant='header6'>Subscription</Text>
+                  <Stack.Item growthFactor={1} shrinkFactor={1} />
+                  <Button variant='secondary' onClicked={onManageWithStripeClicked} text='Manage with Stripe' />
+                </Stack>
+                <Grid shouldAddGutters={true} paddingHorizontal={PaddingSize.Wide} paddingVertical={PaddingSize.Wide}>
+                  {consoleConfig.plans.map((plan: IPlan, index: number): React.ReactElement => {
+                    if (!plan.isPurchasable) {
+                      return null;
+                    }
+                    return (
+                      <Grid.Item sizeResponsive={{ base: 12, small: 6, medium: 4, large: 3 }} key={index}>
+                        <Box variant='card' isFullHeight={true}>
+                          <Stack direction={Direction.Vertical} isFullWidth={false} contentAlignment={Alignment.Start} defaultGutter={PaddingSize.Default} shouldAddGutters={true}>
+                            <Text variant='header6'>{plan.name}</Text>
+                            <Stack.Item growthFactor={1} shrinkFactor={1}>
+                              <Text variant='light'>{plan.highlightFeature.toUpperCase()}</Text>
+                            </Stack.Item>
+                            <Text variant='bold'>{`$${plan.priceMonthly / 100}`}</Text>
+                            <Text variant='light'>per month</Text>
+                            {plan.planIndex < currentPlan.planIndex && <Button variant='secondary' isFullWidth={true} onClicked={(): void => onChangePlanClicked(plan)} text='Downgrade' />}
+                            {plan.planIndex === currentPlan.planIndex && <Button isEnabled={false} variant='secondary' isFullWidth={true} onClicked={(): void => onChangePlanClicked(plan)} text='Your plan' />}
+                            {plan.planIndex > currentPlan.planIndex && <Button variant='primary' isFullWidth={true} onClicked={(): void => onChangePlanClicked(plan)} text='Upgrade' />}
+                          </Stack>
+                        </Box>
+                      </Grid.Item>
+                    );
+                  })}
+                </Grid>
+              </Box>
+            </React.Fragment>
+          )}
+        </Stack>
+      </ResponsiveContainingView>
 
-        {account && (
-          <AccountUpgradeDialog
-            isOpen={isAccountUpgradePopupShowing}
-            account={account}
-            onCloseClicked={onAccountUpgradePopupCloseClicked}
-            onUpgradeClicked={onAccountUpgradePopupUpgradeClicked}
-          />
-        )}
+      {account && (
+        <AccountUpgradeDialog
+          isOpen={isAccountUpgradePopupShowing}
+          account={account}
+          onCloseClicked={onAccountUpgradePopupCloseClicked}
+          onUpgradeClicked={onAccountUpgradePopupUpgradeClicked}
+        />
+      )}
 
-        {newPlan && (
-          <NewPlanDialog
-            isUpgradeDialogLoading={isUpgradeDialogLoading}
-            newPlan={newPlan}
-            currentPlan={currentPlan}
-            upgradeCardError={upgradeCardError}
-            upgradeDiscountCode={upgradeDiscountCode}
-            upgradeDiscountCodeError={upgradeDiscountCodeError}
-            onUpgradeDialogClosed={onUpgradeDialogClosed}
-            onUpgradeDiscountCodeChanged={onUpgradeDiscountCodeChanged}
-            upgradeError={upgradeError}
-            onUpgradeDialogUpgradeClicked={onUpgradeDialogUpgradeClicked}
-          />
-        )}
-        <ToastContainer />
-      </Elements>
-    </div>
+      {newPlan && (
+        <NewPlanDialog
+          isUpgradeDialogLoading={isUpgradeDialogLoading}
+          newPlan={newPlan}
+          currentPlan={currentPlan}
+          upgradeCardError={upgradeCardError}
+          upgradeDiscountCode={upgradeDiscountCode}
+          upgradeDiscountCodeError={upgradeDiscountCodeError}
+          onUpgradeDialogClosed={onUpgradeDialogClosed}
+          onUpgradeDiscountCodeChanged={onUpgradeDiscountCodeChanged}
+          upgradeError={upgradeError}
+          onUpgradeDialogUpgradeClicked={onUpgradeDialogUpgradeClicked}
+        />
+      )}
+      <ToastContainer />
+    </Elements>
   );
 };
