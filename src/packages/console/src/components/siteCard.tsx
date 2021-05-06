f96475a7442e11825707792b156bc5157a4aa982
Moiyad <moiyadkayda@gmail.com>
2021-05-06 16:31:35 +0000

    .

--- src/packages/console/src/components/siteCard.tsx
+++ src/packages/console/src/components/siteCard.tsx
@@ -1,34 +1,10 @@
 import React from 'react';
 
-import Card from '@material-ui/core/Card';
+import { BackgroundView, Box, Direction, Image, PaddingSize, Stack, Text } from '@kibalabs/ui-react';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 
 import { Site } from '../everypageClient/resources';
 
-const useStyles = makeStyles(() => ({
-  siteCardButtonBase: {
-    width: '100%',
-  },
-  siteCardContent: {
-    width: '100%',
-  },
-  siteNameText: {
-    fontWeight: 'bold',
-  },
-  card: {
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  disabledCard: {
-    backgroundColor: '#eee',
-  },
-}));
 
 interface ISiteCardProps {
   site: Site;
@@ -37,30 +13,33 @@ interface ISiteCardProps {
 }
 
 export const SiteCard = (props: ISiteCardProps): React.ReactElement => {
-  const classes = useStyles();
 
   const onSiteClicked = (): void => {
     props.onSiteClicked(props.site);
   };
 
   return (
-    <Card className={props.isEnabled ? classes.card : classes.disabledCard}>
-      <CardActionArea className={classes.siteCardButtonBase} onClick={onSiteClicked} disabled={!props.isEnabled}>
-        <CardMedia
-          className={classes.media}
-          image={`https://${props.site.slug}.evrpg.com/screenshot-preview.png`}
-          title={props.site.name}
-        />
-        <CardContent className={classes.siteCardContent}>
-          <Typography variant='h6' className={classes.siteNameText}>
-            {props.site.name}
-          </Typography>
-          <Typography color='textSecondary'>
-            {props.site.slug}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <BackgroundView color={!props.isEnabled && '#eee'}>
+      <Box variant='card-unpadded'>
+        <CardActionArea style={{ width: '100%' }} onClick={onSiteClicked} disabled={!props.isEnabled}>
+          <Image
+            isFullHeight={false}
+            source={`https://${props.site.slug}.evrpg.com/screenshot-preview.png`}
+            alternativeText={props.site.name}
+          />
+          <Box variant='padded' isFullWidth={true}>
+            <Stack direction={Direction.Vertical} paddingVertical={PaddingSize.Wide1}>
+              <Text tag='h6' variant='bold'>
+                {props.site.name}
+              </Text>
+              <Text variant='light'>
+                {props.site.slug}
+              </Text>
+            </Stack>
+          </Box>
+        </CardActionArea>
+      </Box>
+    </BackgroundView>
   );
 };
 SiteCard.defaultProps = {
