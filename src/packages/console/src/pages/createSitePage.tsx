bdf38c2290afdca98f8f2376c82fc5ab9f2ea148
Krishan Patel <krishan711@gmail.com>
2021-05-17 16:35:57 +0100

    .

--- src/packages/console/src/pages/createSitePage.tsx
+++ src/packages/console/src/pages/createSitePage.tsx
@@ -143,10 +143,11 @@ export const CreateSitePage = (): React.ReactElement => {
                       selectedItemKey={String(selectedAccountId)}
                       inputWrapperVariant={selectedAccountIdError && 'error'}
                       messageText={selectedAccountIdError} onItemClicked={onAccountSelected}
-                      options={(accounts || []).map((account: Account): IOption => (
-                        { itemKey: String(account.accountId),
-                          text: account.name,
-                          textVariant: 'selectItemText' }))}
+                      options={(accounts || []).map((account: Account): IOption => ({
+                        itemKey: String(account.accountId),
+                        text: account.name,
+                        textVariant: 'selectItemText',
+                      }))}
                     />
                     <SingleLineInput
                       id='slug'
