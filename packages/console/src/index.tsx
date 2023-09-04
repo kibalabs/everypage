aac4e9b82b07b3553d66be4dc1900f8390627aa5
Krishan Patel <krishan711@gmail.com>
2023-09-04 11:02:22 +0100

    Fiddle: update deployment to point to new box (#108)
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

--- packages/console/src/index.tsx
+++ packages/console/src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-import ReactDOM from 'react-dom';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 
 import { App } from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const renderedPath = window.KIBA_RENDERED_PATH;
+const pageData = window.KIBA_PAGE_DATA;
+
+if (typeof document !== 'undefined') {
+  const target = document.getElementById('root') as HTMLDivElement;
+  if (target.hasChildNodes() && window.location.pathname === renderedPath) {
+    hydrateRoot(target, <React.StrictMode><App pageData={pageData} /></React.StrictMode>);
+  } else {
+    createRoot(target).render(<React.StrictMode><App pageData={pageData} /></React.StrictMode>);
+  }
+}
